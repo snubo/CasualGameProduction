d9ee0f9e8e30113e8ccfc96ce6df41eabae76c0d
nathanAjacobs <nathanjacobs@Knights.ucf.edu>
2019-10-02 12:10:00 -0400

    Unity project added.

---
+++ New Unity Project/Assets/TrackColliderTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackColliderTrigger : MonoBehaviour
+{
+    public GameObject myPrefab;
+    public Transform newPos;
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Debug.Log("Yoooo");
+            GameObject temp = Instantiate(myPrefab, newPos.position, Quaternion.Euler(newPos.rotation.eulerAngles.x, -newPos.rotation.eulerAngles.y, newPos.rotation.eulerAngles.z));
+
+
+
+            //temp.transform.position = GetComponentInChildren<Transform>().position;
+
+            Debug.Log(GetComponentInChildren<Transform>().gameObject);
+        }
+    }
+}
---
+++ New Unity Project/Assets/TrainMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainMovement : MonoBehaviour
+{
+    public bool triggerHit;
+    public GameObject myPrefab;
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        triggerHit = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        /*if(triggerHit)
+        {
+            Instantiate(myPrefab, GetComponentInChildren);
+            triggerHit = false;
+        }*/
+        rb.MovePosition(rb.position + new Vector3(0, 0, -1));
+    }
+}
